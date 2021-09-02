719745fb437a0f93e5e275c5b5f8f7382bf08839
Emon Ahmed <technicalboyemon@gmail.com>
2021-09-02 22:04:08 +0600

    Style Fixing

--- app.js
+++ app.js
@@ -6,46 +6,40 @@ const searchBook = () => {
   searchText.value = "";
   const bookLoading = document.getElementById("row");
   bookLoading.innerHTML = `
-    <div class="spinner-border" role="status">
+    <div class="spinner-border m-auto" role="status">
         <span class="visually-hidden">Loading...</span>
     </div>
   `;
   fetch(url)
     .then((res) => res.json())
     .then((data) => {
-      showBook(data.docs.slice(0, 20), data.docs.length, data.numFound);
+      showBook(data.docs.slice(0, 24), data.docs.length, data.numFound);
     });
 };
-
 const showBook = (docs, length, totalBook) => {
   const showTitle = document.getElementById("row");
   showTitle.innerHTML = "";
   if (docs.length > 0) {
     document.getElementById(
       "showing"
-    ).innerHTML = `<span class="text-center fw-blod">Total Books: ${totalBook}</span>
-        <span class="text-center fw-blod">Showing Result: ${length}</span>`;
+    ).innerHTML = `
+    <div class="text-center">
+      <span class="fw-bold">Total Books:</span> ${totalBook}
+      <span class="fw-bold">Showing Result:</span> ${length}
+    </div>`;
 
     docs.forEach((book) => {
       document.getElementById("row").innerHTML += `
             <div class="col-md-3">
                 <span>
-                    <div class="card my-5">
-                        <img class="card-img-top" src="
-                        
-                            ${
-                              book.cover_i
-                                ? `https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg`
-                                : `https://upload.wikimedia.org/wikipedia/commons/thumb/6/65/No-Image-Placeholder.svg/1200px-No-Image-Placeholder.svg.png`
-                            }
-                        
-                        "  alt="Card image cap">
+                    <div class="card my-4 shadow">
+                        <img class="card-img-top" 
+                        src="${book.cover_i ? `https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg` : `images/no.png`}" alt="Book Cover Photo">
                         <div class="card-body bg-light">
-                            <p class="card-text">${book.title}</p>
-                            <p class="card-text">${book.first_publish_year}</p>
-                            <p class="card-text">${book.publisher}</p>
-                            <p class="card-text">${book.author_name}</p>
-                            
+                            <p class="card-text fw-bold">${book.title}</p>
+                            <p class="card-text">First Publish Year: ${book.first_publish_year}</p>
+                            <p class="card-text">Puslisher Name: ${book.publisher}</p>
+                            <p class="card-text">Author Name: ${book.author_name}</p>
                         </div>
                     </div>
                 </span>
